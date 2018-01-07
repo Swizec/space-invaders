e039799026ddc0d7c0985c401fa13d59057b1784
Swizec Teller <swizec@swizec.com>
2018-01-07 14:57:29 -0800

    Make enemies shoot

--- src/Constants.jsx
+++ src/Constants.jsx
@@ -1,22 +1,21 @@
-
-export const START_GAME = 'start_game';
-export const STOP_GAME = 'stop_game';
-export const TIME_TICK = 'tick';
-export const CHANGE_EVENT = 'change';
-export const GAME_OVER = 'game_over';
+export const START_GAME = "start_game";
+export const STOP_GAME = "stop_game";
+export const TIME_TICK = "tick";
+export const CHANGE_EVENT = "change";
+export const GAME_OVER = "game_over";
 
 export const EDGE = 10;
 
-export const PLAYER_MOVE = 'player_move';
-export const PLAYER_STOP = 'player_stop';
+export const PLAYER_MOVE = "player_move";
+export const PLAYER_STOP = "player_stop";
 export const PLAYER_MAX_SPEED = 30;
-export const PLAYER_SHOOT = 'player_shoot';
+export const PLAYER_SHOOT = "player_shoot";
 
-export const MOUSE_TRIGGER = 'mouse';
-export const KEY_TRIGGER = 'key';
+export const MOUSE_TRIGGER = "mouse";
+export const KEY_TRIGGER = "key";
 
 export const BULLET_MAX_SPEED = 5;
 
-export const ENEMY_SHOTS_PER_MINUTE = 0;
+export const ENEMY_SHOTS_PER_MINUTE = 50;
 export const ENEMY_RADIUS = 5;
 export const MS_PER_FRAME = 16;
